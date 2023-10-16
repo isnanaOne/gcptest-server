9e50f0a91138a4e9cdb21d311e157a5ae45ca83c
isnanaOne <1onlyonehana@gmail.com>
2023-10-16 09:34:04 +0900

    aa

--- src/test/java/com/example/gcptest/GcptestApplicationTests.java
+++ src/test/java/com/example/gcptest/GcptestApplicationTests.java
@@ -1,9 +1,19 @@
 package com.example.gcptest;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GcptestApplicationTests {
 
+    @Test
+    void test() {
+        Assertions.assertEquals(1+1, 2);
+    }
+
+    @Test
+    void test1() {
+        Assertions.assertEquals(1+10, 10);
+    }
 }
